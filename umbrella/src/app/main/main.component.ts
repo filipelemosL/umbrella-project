5a851dc3d880c0a9f0846e442e2131b380c6fcaa
Filipe Lemos <filipelemos1415@gmail.com>
2025-01-15 00:13:59 -0300

    answer engine complete

--- umbrella/src/app/main/main.component.ts
+++ umbrella/src/app/main/main.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  private apiKey: string = '388a669a8b574f9ca3011722251501'; // Substitua pela sua chave de API
+  private apiUrl: string = 'http://api.weatherapi.com/v1/forecast.json';
+  public willItRain: string = '';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getWeather();
+  }
+
+  getWeather(): void {
+    const params = {
+      key: this.apiKey,
+      q: 'seattle',
+      lang: 'pt'
+    };
+
+    this.http.get(this.apiUrl, { params }).subscribe(
+      (response: any) => {
+        console.log(response);
+        this.checkRain(response);
+      },
+      (error) => {
+        console.error('Erro ao fazer a requisição:', error);
+      }
+    );
+  }
+
+  checkRain(data: any): void {
+    const forecast = data.forecast.forecastday[0];
+    this.willItRain = forecast.day.daily_will_it_rain ? 'Sim' : 'Não';
+  }
 
 }
