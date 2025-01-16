6b6018fe709bfdec831c90486bfa6ab03b6b5d11
Filipe Lemos <filipelemos1415@gmail.com>
2025-01-16 13:49:18 -0300

    location by ip added

--- umbrella/src/app/main/main.component.ts
+++ umbrella/src/app/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main',
@@ -7,26 +8,19 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-  private apiKey: string = '388a669a8b574f9ca3011722251501'; // Substitua pela sua chave de API
+  private apiKey: string = '388a669a8b574f9ca3011722251501'; 
   private apiUrl: string = 'http://api.weatherapi.com/v1/forecast.json';
+  private ipInfoToken: string = '2b82bf8f99ea6c';
   public willItRain: string = '';
+  public location: string = '';
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.getWeather();
-  }
-
-  getWeather(): void {
-    const params = {
-      key: this.apiKey,
-      q: 'seattle',
-      lang: 'pt'
-    };
-
-    this.http.get(this.apiUrl, { params }).subscribe(
+    this.getIpInfo().pipe(
+      switchMap((location: string) => this.getWeather(location))
+    ).subscribe(
       (response: any) => {
-        console.log(response);
         this.checkRain(response);
       },
       (error) => {
@@ -35,9 +29,29 @@ export class MainComponent implements OnInit {
     );
   }
 
+  getIpInfo() {
+    const url = `https://ipinfo.io/json?token=${this.ipInfoToken}`;
+    return this.http.get(url).pipe(
+      switchMap((response: any) => {
+        this.location = response.city;
+        console.log(this.location);
+        return [this.location];
+      })
+    );
+  }
+
+  getWeather(location: string) {
+    const params = {
+      key: this.apiKey,
+      q: location,
+      lang: 'pt'
+    };
+
+    return this.http.get(this.apiUrl, { params });
+  }
+
   checkRain(data: any): void {
     const forecast = data.forecast.forecastday[0];
     this.willItRain = forecast.day.daily_will_it_rain ? 'Sim' : 'Não';
   }
-
 }
